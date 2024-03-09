f37f8539f013a61f21195160aec647fdc38c869b
MaximilianLoessl <maximilian.loessl@students.unibe.ch>
2024-03-09 09:28:30 +0100

    try

--- flight_scraper.py
+++ flight_scraper.py
@@ -1,12 +1,29 @@
-from time import sleep
 import pandas as pd
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
 import os
 
-driver = webdriver.Chrome()
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
+# selenium 4
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
+# options:
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-extensions")
+#chrome to stay open
+options.add_experimental_option("detach", True)
+
+# which drivercommand is it?
+# driver = webdriver.Chrome()
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
 # url = 'https://www.kayak.ch/flights/ZRH,BSL,GVA-KGL/2024-07-11/2students?sort=bestflight_a' # option to add more
 
 #try if this works, add list as to location --> loop over
@@ -14,13 +31,7 @@ to_location = 'KGL'
 url = 'https://www.kayak.ch/flights/ZRH,BSL,GVA-{to_location}/2024-07-11/2students?sort=bestflight_a'.format(to_location=to_location)
 
 driver.get(url)
-sleep(10)
 
-# pop_window = '//*[@id="portal-container"]/div/div[2]/div/div/div[3]/div/div[1]/button[3]/div'
-popup_window = '//*[@id="portal-container"]/div/div[2]/div/div/div[1]/div/span[2]/button/'
-driver.find_element(popup_window).click()
 
-# noch keine möglichkeit gefunden, um die cookies zu schliessen, weil kein spezifischer xpath -->
-
-# hier noch optionen drin zum anpassen, wie dass Seite nicht schliesst
-# https://stackoverflow.com/questions/72835195/accept-bypass-cookies-with-selenium
+popup_path = '//*[@id="portal-container"]/div/div[2]/div/div/div[1]/div/span[2]/button'
+WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, popup_path))).click()
